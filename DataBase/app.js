e6bcfd173b596e5c9770d2224a72f91292ecc6fa
swapnil_1800 <swapnilsureshgupta365@gmail.com>
2026-02-21 12:19:24 +0530

    new push new database

--- DataBase/app.js
+++ DataBase/app.js
@@ -1,9 +1,38 @@
  const express = require('express');
  const app = express();
+ const usermodel = require('./usermodel');
 
  app.get('/',(req,res)=>{
     res.send("hey");
  })
+
+ app.get('/create',async (req,res)=>{
+    let createduser =  await usermodel.create({
+       name:"swapnil",
+       username:"swapnil",
+       email:"swapnil@gmail.com"
+
+    })
+    res.send(createduser); 
+ }) 
+
+//   app.get('/update',async (req,res)=>{
+//    //  const updateduser =  usermodel.findOneAndUpdate({findone}, {update}, {new: true}) 
+//        const updateduser =  usermodel.findOneAndUpdate({username:"swapnil"}, {name:"swapnil suresh gupta"}, {new: true}) 
+//       res.render(createduser); 
+//     })
+
+//    app.get("/read", async (req, res) => {
+//    //   let users = await userModel.findOne({username: "harsh"});
+//      let users = await usermodel.find();
+//      res.send(users);
+//      })
+
+//    app.get("/delete", async (req, res) => {
+//     let users = await usermodel.findOneAndDelete({username: "harsh"})
+//      res.send(users);
+//      })
+// I
 
 
  app.listen(3000);
